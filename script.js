a4a7c980ad8f0ad64c2623e0e946dfc05a194efa
Jameson Buie <jamesonbuie@gmail.com>
2022-02-14 10:33:21 -0600

    Completed JS Basics Exercise

---
+++ script.js
@@ -0,0 +1,52 @@
+// JS BASICS EXERCISE   
+// 1
+console.log(17);
+
+// 2
+// Numbers, Strings, Null, Booleans, Undefined
+// Symbols are also a primitive. However, they are not commonly used
+
+// 3
+// +, -, /, *, **, %
+
+// 4
+console.log(5 % 2);
+console.log(5 % 4);
+
+// 5
+console.log('I\'m Mary Poppins, y\'all!');
+
+// 6
+console.log("Hawk" + "eye");//Hawkeye
+
+//7
+console.log("Jameson".length + "Buie".length);//11
+
+// 8
+console.log("Avengers: Endgame"[16]);//e
+
+// BONUS
+// 1
+console.log("\\");
+
+// 2
+console.log(5**4-9);//616
+
+// 3
+console.log("I".length ** "Am".length - "Iron".length + "Man".length);
+
+// 4 
+console.log("CAP" [1]);
+console.log("VISION" [0]);
+console.log("OKOYE" [4]);
+console.log("NICK FURY" [0]);
+console.log("GROOT" [0]);
+console.log("DOCTOR STRANGE" [13]);
+console.log("THOR" [3]);
+console.log("STAR-LORD" [0]);
+
+// 5
+m=25;
+c=4;
+u=m+c;
+console.log(u)//29
